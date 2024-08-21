69250cbd858f506659e76ac701bc3613af2924e7
Roy Nieterau <roy_nieterau@hotmail.com>
2024-08-22 01:52:38 +0200

    Collect required data for local and farm render jobs to have a 'colorspace' value specified

--- client/ayon_houdini/api/colorspace.py
+++ client/ayon_houdini/api/colorspace.py
@@ -1,59 +1,55 @@
+from typing import List
+
 import attr
 import hou
 from ayon_houdini.api.lib import get_color_management_preferences
-from ayon_core.pipeline.colorspace import get_display_view_colorspace_name
+from ayon_core.pipeline.colorspace import (
+    get_display_view_colorspace_name,
+    get_ocio_config_colorspaces
+)
+
 
 @attr.s
 class LayerMetadata(object):
     """Data class for Render Layer metadata."""
-    frameStart = attr.ib()
-    frameEnd = attr.ib()
+    products: "List[RenderProduct]" = attr.ib()
 
 
 @attr.s
 class RenderProduct(object):
-    """Getting Colorspace as
-    Specific Render Product Parameter for submitting
-    publish job.
-
-    """
+    """Specific Render Product Parameter for submitting."""
     colorspace = attr.ib()                      # colorspace
-    view = attr.ib()
     productName = attr.ib(default=None)
 
 
 class ARenderProduct(object):
-
-    def __init__(self):
-        """Constructor."""
-        # Initialize
-        self.layer_data = self._get_layer_data()
-        self.layer_data.products = self.get_colorspace_data()
-
-    def _get_layer_data(self):
-        return LayerMetadata(
-            frameStart=int(hou.playbar.frameRange()[0]),
-            frameEnd=int(hou.playbar.frameRange()[1]),
-        )
-
-    def get_colorspace_data(self):
-        """To be implemented by renderer class.
-
-        This should return a list of RenderProducts.
-
-        Returns:
-            list: List of RenderProduct
-
-        """
-        data = get_color_management_preferences()
-        colorspace_data = [
-            RenderProduct(
-                colorspace=data["display"],
-                view=data["view"],
-                productName=""
-            )
+    """This is the minimal data structure required to get
+    `ayon_core.pipeline.farm.pyblish_functions.create_instances_for_aov` to
+    work with deadline addon's job submissions."""
+    # TODO: The exact data structure should actually be defined in core for all
+    #  addons to align.
+    def __init__(self, aov_names: List[str]):
+        colorspace = get_scene_linear_colorspace()
+        products = [
+            RenderProduct(colorspace=colorspace, productName=aov_name)
+            for aov_name in aov_names
         ]
-        return colorspace_data
+        self.layer_data = LayerMetadata(products=products)
+
+
+def get_scene_linear_colorspace():
+    """Return colorspace name for Houdini's OCIO config scene linear role.
+
+    By default, renderers in Houdini render output images in the scene linear
+    role colorspace.
+
+    Returns:
+        Optional[str]: The colorspace name for the 'scene_linear' role in
+            the OCIO config Houdini is currently set to.
+    """
+    ocio_config_path = hou.Color.ocio_configPath()
+    colorspaces = get_ocio_config_colorspaces(ocio_config_path)
+    return colorspaces["roles"].get("scene_linear", {}).get("colorspace")
 
 
 def get_default_display_view_colorspace():
