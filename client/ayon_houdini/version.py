d70489d40531e43fb3abe3d2fa0f44f292de1f6b
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2024-09-06 11:16:45 +0200

    bump version to '0.3.12'

--- client/ayon_houdini/version.py
+++ client/ayon_houdini/version.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 """Package declaring AYON addon 'houdini' version."""
-__version__ = "0.3.12-dev.1"
+__version__ = "0.3.12"
--- package.py
+++ package.py
@@ -1,6 +1,6 @@
 name = "houdini"
 title = "Houdini"
-version = "0.3.12-dev.1"
+version = "0.3.12"
 
 client_dir = "ayon_houdini"
 
