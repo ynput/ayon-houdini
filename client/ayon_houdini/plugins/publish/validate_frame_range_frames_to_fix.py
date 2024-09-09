3cf568e7ccd81085738ad72279680db7762e8011
Roy Nieterau <roy_nieterau@hotmail.com>
2024-09-09 18:02:44 +0200

    Cosmetics + restructure code to opt out earlier when we can

--- client/ayon_houdini/plugins/publish/validate_frame_range_frames_to_fix.py
+++ client/ayon_houdini/plugins/publish/validate_frame_range_frames_to_fix.py
@@ -29,8 +29,8 @@ class ValidateFrameRangeFramesToFix(plugin.HoudiniInstancePlugin):
                 "Invalid Rop Frame Range",
                 description=(
                     "## Invalid Rop Frame Range\n"
-                    "Invalid frame range because the instance "
-                    "frame range [{0[frameStart]} - {0[frameEnd]}] doesn't cover "
+                    "Invalid frame range because the instance frame range "
+                    "[{0[frameStart]} - {0[frameEnd]}] doesn't cover "
                     "the frames to fix [{0[frames_to_fix]}]."
                     .format(instance.data)
                 )
@@ -38,34 +38,36 @@ class ValidateFrameRangeFramesToFix(plugin.HoudiniInstancePlugin):
     
     @classmethod
     def get_invalid(cls, instance):
-
         if not instance.data.get("instance_node"):
             return
 
-        rop_node = hou.node(instance.data["instance_node"])
-        frame_start = instance.data.get("frameStartHandle") 
-        frame_end = instance.data.get("frameEndHandle")
-
-        frames_to_fix = instance.data.get("frames_to_fix", "")
+        frames_to_fix: str = instance.data.get("frames_to_fix", "")
         if not frames_to_fix:
-            cls.log.debug("Skipping Validation, No frames to fix.")
+            cls.log.debug("Skipping Validation, no frames to fix.")
             return
 
+        rop_node = hou.node(instance.data["instance_node"])
+        frame_start = instance.data["frameStartHandle"]
+        frame_end = instance.data["frameEndHandle"]
+
         frames_to_fix = clique.parse(frames_to_fix, "{ranges}")
+        fix_frame_start = int(frames_to_fix[0])
+        fix_frame_end = int(frames_to_fix[-1])
 
         # Check if ROP frame range covers the frames to fix.
-        frame_range = list(frames_to_fix)
         # Title and message are the same for the next two checks.
-        if not frame_start <= int(frame_range[0]):
+        if frame_start > fix_frame_start:
             cls.log.error(
-                "Start frame should be smaller than or equal the first frame to fix."
-                f"Setting the start frame to the first frame to fix {int(frame_range[0])}."
+                "Start frame should be smaller than or equal to the first "
+                "frame to fix. Set the start frame to the first frame to fix: "
+                f"{fix_frame_start}."
             )
             return rop_node.path()
 
-        if not frame_end >= int(frame_range[-1]):
+        if frame_end < fix_frame_end:
             cls.log.error(
-                "End frame should be greater than or equal the last frame to fix."
-                f"Setting the end frame to the last frame to fix {int(frame_range[-1])}."
+                "End frame should be greater than or equal to the last frame "
+                "to fix. Set the end frame to the last frame to fix: "
+                f"{fix_frame_end}."
             )
             return rop_node.path()
