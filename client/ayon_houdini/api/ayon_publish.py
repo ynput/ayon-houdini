22eefc4e1d92b68c3311207ed429300b27f044db
Roy Nieterau <roy_nieterau@hotmail.com>
2024-11-11 22:47:02 +0100

    Cleanup code

--- client/ayon_houdini/api/ayon_publish.py
+++ client/ayon_houdini/api/ayon_publish.py
@@ -1,6 +1,5 @@
 import logging
 import re
-import os
 
 import hou
 import pyblish
@@ -8,11 +7,6 @@ import pyblish
 from ayon_houdini.api import lib
 from ayon_core.pipeline import registered_host
 from ayon_core.pipeline.create import CreateContext
-
-
-# TODO: Remove when testing
-import importlib
-importlib.reload(lib)
 
 
 def publish(node_path: str):
@@ -48,7 +42,9 @@ def publish(node_path: str):
     pyblish_plugins = create_context.publish_plugins
 
     error_format = "Failed {plugin.__name__}: {error} -- {error.traceback}"
-    # filter out the increment plugin because it will cause our export to get out of sync with later run nodes
+
+    # filter out the increment plugin because it will cause our export to
+    # get out of sync with nodes running later downstream
     filtered_plugins = [
         plugin
         for plugin in pyblish_plugins
@@ -56,7 +52,6 @@ def publish(node_path: str):
     ]
 
     for result in pyblish.util.publish_iter(pyblish_context, filtered_plugins):
-
         log.debug(result)
         for record in result["records"]:
             log.debug("{}: {}".format(result["plugin"].label, record.msg))
@@ -86,65 +81,59 @@ def set_ayon_publish_nodes_pre_render_script(
             node.parm("prerender").set(val)
 
 
-frame_var_regx = r"\$F(?:\d*|F)"
+def get_rop_output(rop: hou.RopNode, frame_range=None) -> list:
+    """Returns a list of output files a RopNode will generate on render.
 
-
-def get_rop_output(rop: hou.RopNode) -> list:
-    """returns a list of output files a Given node will generated based on Frame range and replacement Varaibles
+    It will use the frame range and replacement variables.
 
     Args:
-        rop: input Rop Node
+        rop: Rop Node to get output files for.
+        frame_range: Optional frame range tuple (start, end, [increment]).
+            If not provided, it will use the frame range set on the RopNode.
 
-
-
-    Returns (list:str): list of strings with the output files. Empty list if its not possible to evaluate the output
-
+    Returns:
+         List[str]: List of output files from the ROP node.
     """
-    out_parm = lib.get_output_parameter(rop)
-    rop = out_parm.node()
-
     renders_range = rop.parm("trange").eval()
+    out_parm = lib.get_output_parameter(rop)
 
-    if 0 == renders_range:  # Render Current Frame
+    # Render Current Frame
+    if frame_range is None and renders_range == 0:
         return [out_parm.eval()]
-        # just evaluate the parm
-    elif (
-        1 == renders_range or 2 == renders_range
-    ):  # Render Frame Range or Render Frame Range Only
-        raw_out = out_parm.rawValue()
 
-        match = re.findall(frame_var_regx, raw_out)
-        if not match:
-            # if no Range parms exist we just return the eveluated parm
-            return [out_parm.eval()]
+    # Render Frame Range or Render Frame Range Only
+    # Get the file path with $F as format-able part as path template.
+    path = lib.evalParmNoFrame(rop, out_parm.name())
+    if "#" not in path:
+        # Not frame dependent filename, single file
+        return [path]
 
+    def replace(match):
+        padding = len(match.group(0))
+        if padding < 2:
+            return "{frame}"
+        else:
+            return "{{frame:0{padding}d}}".format(padding=padding)
+
+    path_template = re.sub("#+", replace, path)
+
+    inc = rop.parm("f3").eval()
+    if frame_range is not None:
+        # Use user specified frame range tuple (2 or 3 elements)
+        # See Houdini `RopNode.render` arguments
+        start = frame_range[0]
+        end = frame_range[1]
+        if len(frame_range) == 3:
+            inc = frame_range[2]
+    else:
         start = rop.parm("f1").eval()
         end = rop.parm("f2").eval()
-        inc = rop.parm("f3").eval()
-        out_list = []
-        # TODO dose this need to be O(n*m) ?
 
-        # TODO find out why expandString replaces $OS with Director and not the actual node name
-        # https://www.sidefx.com/docs/houdini/network/expressions.html#globals
-        os_replaced_out = raw_out.replace("$OS", rop.name())
-        for frame in range(int(start), int(end), int(inc)):
-            frame_out_parm = os_replaced_out
+    files = []
+    for frame in range(int(start), int(end), int(inc)):
+        files.append(path_template.format(frame=frame))
 
-            # TODO this dose not handle the different frame padding options correctly
-            for i in match:
-                num = re.findall(r"\d+", i)[0]
-                frame_out_parm = frame_out_parm.replace(i, str(frame).zfill(int(num)))
-
-            frame_out_parm = hou.expandString(frame_out_parm)
-            if not os.path.exists(
-                frame_out_parm
-            ):  # TODO this should probably check the time stamp to avoid publishing files from old exports into the same dir
-                continue
-            out_list.append(frame_out_parm)
-
-        return out_list
-
-    return []
+    return files
 
 
 def ayon_publish_command():
