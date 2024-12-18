f2e6c68d2fa8f6a45ac311ffa366bee21007451a
MustafaJafar <mustafataherzaky@outlook.com>
2024-12-18 18:55:11 +0200

    add `default_output_dir` general setting

--- server/settings/general.py
+++ server/settings/general.py
@@ -28,6 +28,14 @@ class GeneralSettingsModel(BaseSettingsModel):
         False,
         title="Add Self Publish Button"
     )
+    default_output_dir: str = SettingsField(
+        title="Default Output Directory",
+        description="This is the initial output directory for newly created "
+                    "AYON ROPs. It serves as a starting point when a new ROP "
+                    "is generated using the AYON creator. Artists can modify "
+                    "this directory after the ROP is created. "
+                    "Note: AYON creator will expand any Houdini vars."
+    )
     update_houdini_var_context: UpdateHoudiniVarcontextModel = SettingsField(
         default_factory=UpdateHoudiniVarcontextModel,
         title="Update Houdini Vars on context change"
@@ -36,6 +44,7 @@ class GeneralSettingsModel(BaseSettingsModel):
 
 DEFAULT_GENERAL_SETTINGS = {
     "add_self_publish_button": False,
+    "default_output_dir": "$HIP/ayon",
     "update_houdini_var_context": {
         "enabled": True,
         "houdini_vars": [
