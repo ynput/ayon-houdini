5e4274dca635578896a3aee5914d8939e2f7e5bb
Roy Nieterau <roy_nieterau@hotmail.com>
2025-07-23 01:00:04 +0200

    `CreateStaticMesh`: Do not inherit `description` from `CreateFBX`

--- client/ayon_houdini/plugins/create/create_fbx.py
+++ client/ayon_houdini/plugins/create/create_fbx.py
@@ -191,12 +191,13 @@ class CreateFBX(plugin.HoudiniCreator):
 
 
 class CreateStaticMesh(CreateFBX):
-    """Static Meshes as FBX."""
+    """Create static meshes as FBX, usually used for Unreal Engine"""
 
     identifier = "io.openpype.creators.houdini.staticmesh.fbx"
     label = "Static Mesh (FBX)"
     product_type = "staticMesh"
     icon = "cube"
+    description = __doc__
 
     def get_publish_families(self):
         return ["fbx", "staticMesh"]
