9252ac030b51793c09197527a00944e6d62ca658
MustafaJafar <mustafataherzaky@outlook.com>
2025-09-11 16:26:55 +0300

    PR #300: Update core addon required version

--- package.py
+++ package.py
@@ -7,7 +7,7 @@ project_can_override_addon_version = True
 
 ayon_server_version = ">=1.1.2"
 ayon_required_addons = {
-    "core": ">=1.0.8",
+    "core": ">1.6.0",
 }
 ayon_compatible_addons = {
     "deadline": ">=0.5.11",
