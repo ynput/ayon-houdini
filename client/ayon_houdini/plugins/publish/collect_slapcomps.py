393fc3945f1a7b461b991314e5e8cc47c8980ed3
MustafaJafar <mustafataherzaky@outlook.com>
2025-09-11 16:48:51 +0300

    CollectSlapComps: better readable code

--- client/ayon_houdini/plugins/publish/collect_slapcomps.py
+++ client/ayon_houdini/plugins/publish/collect_slapcomps.py
@@ -38,55 +38,56 @@ class CollectSlapComps(plugin.HoudiniInstancePlugin):
         comp_numbers = ropnode.evalParm("husk_slapcomp")
         for i in range(1, comp_numbers+1):
         
-            if ropnode.evalParm(f'husk_sc_enable{i}'):
+            if not ropnode.evalParm(f'husk_sc_enable{i}'):
+                continue
 
-                # slap comp cli expects a path to apex node.
-                if ropnode.evalParm(f'husk_sc_source{i}') != "file":
-                    self.log.warning(
-                        "Slap Comp on farm only works with slap files!"
-                    )
-                    continue
-                
-                slapcomp_src = ropnode.evalParm(f'husk_sc_file{i}')
-                
-                name = ropnode.evalParm(f'husk_sc_label{i}')
-               
-                mapinput = []
-
-                for j in range(1, ropnode.evalParm(f'husk_sc_mapinput{i}')+1):
-                    src_aov = ropnode.evalParm(f'husk_sc_in{i}_aov{j}')
-                    dst_cop = ropnode.evalParm(f'husk_sc_in{i}_cop{j}')
-                    if src_aov or dst_cop:
-                        mapinput.append(f"{src_aov}:{dst_cop}")
-
-                mapinput = ",".join(mapinput)
-
-                mapoutput = []
-                for j in range(1, ropnode.evalParm(f'husk_sc_mapoutput{i}')+1):
-                    src_cop = ropnode.evalParm(f'husk_sc_out{i}_cop{j}')
-                    dst_aov = ropnode.evalParm(f'husk_sc_out{i}_aov{j}')
-                    if src_cop or dst_aov:
-                        mapoutput.append(f"{src_cop}:{dst_aov}")
-
-                mapoutput = ",".join(mapoutput)
-                
-                options = {
-                    "name": name,
-                    "mapinput": mapinput,
-                    "mapoutput": mapoutput,
-                }
-
-                options = "&".join(
-                    f"{option}={value}" for option, value in options.items() if value
+            # slap comp cli expects a path to apex node.
+            if ropnode.evalParm(f'husk_sc_source{i}') != "file":
+                self.log.warning(
+                    "Slap Comp on farm only works with slap files!"
                 )
+                continue
+            
+            slapcomp_src = ropnode.evalParm(f'husk_sc_file{i}')
+            
+            name = ropnode.evalParm(f'husk_sc_label{i}')
+            
+            mapinput = []
 
-                if options: 
-                    slapcomp_src += f"?{options}"
+            for j in range(1, ropnode.evalParm(f'husk_sc_mapinput{i}')+1):
+                src_aov = ropnode.evalParm(f'husk_sc_in{i}_aov{j}')
+                dst_cop = ropnode.evalParm(f'husk_sc_in{i}_cop{j}')
+                if src_aov or dst_cop:
+                    mapinput.append(f"{src_aov}:{dst_cop}")
 
-                self.log.debug(
-                    f"Found Slap Comp: {slapcomp_src}"
-                )
-                
-                slapcomp_sources.append(slapcomp_src)
+            mapinput = ",".join(mapinput)
+
+            mapoutput = []
+            for j in range(1, ropnode.evalParm(f'husk_sc_mapoutput{i}')+1):
+                src_cop = ropnode.evalParm(f'husk_sc_out{i}_cop{j}')
+                dst_aov = ropnode.evalParm(f'husk_sc_out{i}_aov{j}')
+                if src_cop or dst_aov:
+                    mapoutput.append(f"{src_cop}:{dst_aov}")
+
+            mapoutput = ",".join(mapoutput)
+            
+            options = {
+                "name": name,
+                "mapinput": mapinput,
+                "mapoutput": mapoutput,
+            }
+
+            options = "&".join(
+                f"{option}={value}" for option, value in options.items() if value
+            )
+
+            if options: 
+                slapcomp_src += f"?{options}"
+
+            self.log.debug(
+                f"Found Slap Comp: {slapcomp_src}"
+            )
+            
+            slapcomp_sources.append(slapcomp_src)
 
         instance.data["slapComp"] = slapcomp_sources
