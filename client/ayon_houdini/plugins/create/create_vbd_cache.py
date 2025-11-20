e48ea81de22b33a5548285c7ea46920993cdd2f7
Mustafa Zaky Jafar <mustafataherzaky@outlook.com>
2025-11-20 10:56:24 +0200

    `CreateVDBCache`: use correct publish families.
    Co-authored-by: Roy Nieterau <roy_nieterau@hotmail.com>

--- client/ayon_houdini/plugins/create/create_vbd_cache.py
+++ client/ayon_houdini/plugins/create/create_vbd_cache.py
@@ -19,7 +19,7 @@ class CreateVDBCache(plugin.HoudiniCreator):
     render_target = "local"
 
     def get_publish_families(self):
-        return ["pointcache", "abc", "publish.hou"]
+        return ["vdbcache", "publish.hou"]
 
     def create(self, product_name, instance_data, pre_create_data):
         import hou
