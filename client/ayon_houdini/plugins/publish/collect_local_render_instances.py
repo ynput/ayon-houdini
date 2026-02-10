6b2db77f7a58e08cc5dd7b8d5b0872aa2654d816
MustafaJafar <mustafataherzaky@outlook.com>
2026-02-10 12:54:22 +0200

    CollectLocalRenderInstances: Better code structure

--- client/ayon_houdini/plugins/publish/collect_local_render_instances.py
+++ client/ayon_houdini/plugins/publish/collect_local_render_instances.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 import pyblish.api
 from ayon_core.pipeline.publish import (
     get_plugin_settings,
@@ -29,8 +30,16 @@ class CollectLocalRenderInstances(plugin.HoudiniInstancePlugin,
 
     label = "Collect local render instances"
 
-    # Non-picklable instance data to transfer to AOV instances.
-    transfer_keys = {}
+    # These will be safely deep copied
+    transfer_keys = {
+        "creator_attributes",
+        "publish_attributes"
+    }
+    # These will transfer, but won't be a unique copy
+    # and are passed by reference.
+    transfer_transient_data_keys = {
+        "instance_node"
+    }
 
     use_deadline_aov_filter = False
     aov_filter = {"host_name": "houdini",
@@ -87,24 +96,8 @@ class CollectLocalRenderInstances(plugin.HoudiniInstancePlugin,
             families_transfer=[],
             instance_transfer={},
         )
-        # Remove frame data like frameStart and handleStart
-        # as they are added in later publisher plugins
-        for key in (
-            "frameStart", "frameEnd",
-            "handleStart", "handleEnd",
-        ):
-            instance_skeleton_data.pop(key, None)
 
-        # `create_skeleton_instance` only adds `render` and `review`
-        # Houdini local render also needs `render.local.hou`
-        instance_skeleton_data["families"].append("render.local.hou")
-        instance_skeleton_data.update({
-            "frameStartHandle": instance.data["frameStartHandle"],
-            "frameEndHandle": instance.data["frameEndHandle"],
-            "instance_node": instance.data["instance_node"],
-            "creator_attributes": instance.data["creator_attributes"],
-            "publish_attributes": instance.data["publish_attributes"],
-        })
+        self.post_process_skeleton_data(instance, instance_skeleton_data)
 
         # Create Instance for each AOV.
         aov_instances = create_instances_for_aov(
@@ -115,16 +108,18 @@ class CollectLocalRenderInstances(plugin.HoudiniInstancePlugin,
             do_not_add_review=False  # Don't explicitly skip review.
         )
 
-        # Add non-picklable instance data to AOV instances after creation
-        if self.transfer_keys:
-            for aov_instance in aov_instances:
-                for key in self.transfer_keys:
-                    if key in instance.data:
-                        aov_instance[key] = instance.data[key]
-
         # Create instances for each AOV
         anatomy = instance.context.data["anatomy"]
         for aov_instance_data in aov_instances:
+            # Copy instance data to AOV instances after creation
+            for key in self.transfer_keys:
+                if key in instance.data:
+                    aov_instance_data[key] = deepcopy(instance.data[key])
+
+            for key in self.transfer_transient_data_keys:
+                if key in instance.data:
+                    aov_instance_data[key] = instance.data[key]
+
             # The `create_instances_for_aov` makes some paths rootless paths,
             # like the "stagingDir" for each representation which we will make
             # absolute again.
@@ -149,3 +144,31 @@ class CollectLocalRenderInstances(plugin.HoudiniInstancePlugin,
         # Skip integrating original render instance.
         # We are not removing it because it's used to trigger the render.
         instance.data["integrate"] = False
+
+    def post_process_skeleton_data(
+            instance: pyblish.api.Instance,
+            instance_skeleton_data: dict
+        ):
+        """Post process skeleton data
+
+        Applies Houdini specific logic to skeleton data.
+        Args:
+            instance (pyblish.api.Instance): The publish instance.
+            instance_skeleton_data (dict): data to modify.
+        """
+
+        # Remove frame data like frameStart and handleStart
+        # as they are added in later publisher plugins
+        for key in (
+            "frameStart", "frameEnd",
+            "handleStart", "handleEnd",
+        ):
+            instance_skeleton_data.pop(key, None)
+
+        # `create_skeleton_instance` only adds `render` and `review`
+        # Houdini local render also needs `render.local.hou`
+        instance_skeleton_data["families"].append("render.local.hou")
+        instance_skeleton_data.update({
+            "frameStartHandle": instance.data["frameStartHandle"],
+            "frameEndHandle": instance.data["frameEndHandle"],
+        })
