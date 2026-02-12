526718545ead485ea4ef01f14f44d6e8d2909954
Ondrej Samohel <ondrej.samohel@ynput.io>
2026-02-12 09:43:11 +0100

    ⤴️ bump up the dependency on ayon-core

--- package.py
+++ package.py
@@ -7,7 +7,7 @@ project_can_override_addon_version = True
 
 ayon_server_version = ">=1.1.2"
 ayon_required_addons = {
-    "core": ">=1.6.11",
+    "core": ">=1.8.0",
 }
 ayon_compatible_addons = {
     "deadline": ">=0.5.20",
